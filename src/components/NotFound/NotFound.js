c581fd12f9bbdf7f12d4a79a0d2bcf3f8aa50b1d
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-17 22:38:07 +0600

    404 page added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='text-danger d-flex flex-column  align-items-center'>
+            <h1 >404</h1>
+            <h1 >Page not found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
