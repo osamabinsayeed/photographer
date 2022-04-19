8ff048e3e359a8bda469185abbbd1f998af1d1a9
osamabinsayeed <osamabinsayeed33@gmail.com>
2022-04-19 21:26:59 +0600

    toasts added

--- src/components/Register/Login.js
+++ src/components/Register/Login.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
@@ -12,13 +15,19 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const customId = "custom-id-yes";
+    if (error) {
+        toast(error?.message, {
+            toastId: customId
+        })
+    }
     return (
         <div className='d-flex justify-content-center mt-5'>
             <Form className='w-50'>
                 <h1 className='text-center'>Login</h1>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -26,12 +35,14 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                    <Form.Control onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required />
                 </Form.Group>
 
                 <Button onClick={() => signInWithEmailAndPassword(email, password)} variant="dark" type="submit">
                     Submit
                 </Button>
+
+                <ToastContainer></ToastContainer>
             </Form>
         </div>
     );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [email, setEmail] = useState('');
@@ -12,18 +16,31 @@ const Register = () => {
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+    const customId = "custom-id-yes";
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+    if (user) {
+        navigate(from);
+    }
+    if (error) {
+        toast(error?.message, {
+            toastId: customId
+        })
+    }
+
     return (
         <div className='d-flex justify-content-center mt-5'>
             <Form className='w-50'>
                 <h1 className='text-center'>Register</h1>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Name:</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your name" />
+                    <Form.Control type="text" placeholder="Enter your name" required />
 
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                    <Form.Control onBlur={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -31,14 +48,20 @@ const Register = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+                    <Form.Control onBlur={(e) => setPassword(e.target.value)} type="password" placeholder="Password" required />
                 </Form.Group>
 
-                <Button variant="dark" type="submit" onClick={() => createUserWithEmailAndPassword(email, password)}>
+                <Button variant="dark" type="submit" onClick={(e) => {
+                    e.preventDefault();
+                    createUserWithEmailAndPassword(email, password)
+                }}>
                     Submit
                 </Button>
+                {/* {error && <p>{error?.message}</p>} */}
+
             </Form>
-        </div>
+            <ToastContainer></ToastContainer>
+        </div >
     );
 };
 
